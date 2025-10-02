6468923a9bfa3cff16d1cd3a7d30385852c97ec5
EduardoCastañedaGonzález <eduardocastanedaglz@gmail.com>
2025-10-02 03:01:29 -0600

    Add files via upload

---
+++ Practicas/practica2/Auto.java
@@ -0,0 +1,27 @@
+public class Auto {
+    private String marca;
+    private String modelo;
+    private int año;
+    private String color;
+    public Auto(String marca, String modelo, int año, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.color = color;
+    }
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
---
+++ Practicas/practica6/Recursivo.java
@@ -0,0 +1,12 @@
+package practica6;
+public class Recursivo {
+    public static void main(String[] args) {
+        if (n<10){
+            System.out.println(n+"" +cadena);
+            mensaje (cadena+ "_", n+1);
+        }
+    }
+    public static void main(String[] args) {
+        mensaje (caena:"_", n:1);
+    }
+}    
